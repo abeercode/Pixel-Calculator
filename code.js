6dcfafa8454ceae2565e7ce2710ad5fe28a7c68f
abeerfedo <adamabeer44@gmail.com>
2025-07-16 23:57:23 +0300

    connect buttons with calculator
    write the code to identify each button and call the appropriate function

--- code.js
+++ code.js
@@ -19,16 +19,29 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(a, op, b) {
-    switch (op) {
-        case "+": add(a, b); break;
-        case "-": subtract(a, b); break;
-        case "*": multiply(a, b); break;
-        case "/": divide(a, b); break;
-    }
+function reminder(a, b) {
+
+    return a % b;
 }
 
+function operate(a, op, b) {
+
+    switch (op) {
+        case "+": return add(a, b); break;
+        case "-": return subtract(a, b); break;
+        case "*": return multiply(a, b); break;
+        case "/": return divide(a, b); break;
+        case "%": return reminder(a, b); break;
+        default: return "ERR";
+    }
+}
+const input = document.querySelector(".input")
+const result = document.querySelector(".result")
 const buttons = document.querySelectorAll(".buttons");
+let firstVal;
+let secondVal;
+let operator;
+
 buttons.forEach((b) => {
 
     const baseName = b.dataset.img;
@@ -43,4 +56,44 @@ buttons.forEach((b) => {
     b.addEventListener("mouseleave", () => {
         b.style.backgroundImage = 'url("' + defaultImg + '")';
     })
+
+    // ------------------------------------------------------------
+    const type = b.dataset.type;
+    const value = b.dataset.value;
+    b.addEventListener("click", () => {
+        if (type == "number") {
+            console.log(firstVal)
+            if (!(firstVal == undefined) && secondVal == undefined) {
+                console.log(firstVal)
+                secondVal = value;
+            }
+            else if (secondVal) {
+                secondVal.append(value)
+            }
+            input.append(value)
+        }
+        if (type == "operator") {
+            operator = value;
+            firstVal = input.textContent;
+            input.append(value)
+
+
+        }
+        if (type == "clear") {
+            if (value == "ac") {
+                input.textContent = ""
+            }
+            else if (value == "c") {
+
+                input.textContent = input.textContent.slice(0, -1)
+            }
+        }
+        if (type == "equal") {
+            console.log(firstVal)
+            console.log(secondVal)
+            result.textContent = operate(parseFloat(firstVal), operator, parseFloat(secondVal))
+
+        }
+    }
+    )
 })
